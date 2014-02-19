d2789ebf314cde002baa9e7382becd8243128854
Patrick Killian <pkillian@berkeley.edu>
2014-02-19 14:55:21 -0800

    Prepare for Gunicorn prod config

---
+++ BearClubs/settings.dev.py
@@ -0,0 +1,14 @@
+try:
+    from shared_settings import *
+except ImportError:
+    pass
+
+# Database
+# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
