1d10709606fb43f63c8fef091641f4890b2a4e6d
Patrick Killian <pkillian@berkeley.edu>
2014-03-12 16:31:07 -0700

    Signup CSS hack to hide spans

--- BearClubs/bc/forms/user.py
+++ BearClubs/bc/forms/user.py
@@ -6,8 +6,7 @@ from BearClubs.bc.models import User
 class UserSignUpForm(UserCreationForm):
     username     = forms.CharField(required=True);
     email        = forms.EmailField(required=True);
-    password     = forms.CharField(required=True, widget=forms.PasswordInput);
-    confirm_pass = forms.CharField(required=True, widget=forms.PasswordInput);
+    #password     = forms.CharField(required=True, widget=forms.PasswordInput);
 
     def clean_username(self):
         # TODO: check that username doesn't exist
@@ -15,8 +14,8 @@ class UserSignUpForm(UserCreationForm):
         return self.cleaned_data.get('username');
 
     def clean_password(self):
-        password     = self.cleaned_data.get('password');
-        confirm_pass = self.cleaned_data.get('confirm_pass');
+        password     = self.cleaned_data.get('password1');
+        confirm_pass = self.cleaned_data.get('password2');
 
         if not confirm_pass:
             raise forms.ValidationError("You must confirm your password");
@@ -27,10 +26,13 @@ class UserSignUpForm(UserCreationForm):
         return password;
 
     def save(self, commit=True):   
-        user            = super(UserSignUpForm, self).save(commit=False)
+        user            = super(UserSignUpForm, self).save(commit=False);
+
+        print self.cleaned_data;
+
         user.username   = self.cleaned_data['username'];
         user.email      = self.cleaned_data['email'];
-        user.password   = self.cleaned_data['password'];
+        user.password   = self.cleaned_data['password1'];
 
         if commit:
             user.save();
@@ -39,4 +41,4 @@ class UserSignUpForm(UserCreationForm):
 
     class Meta:
         model = User;
-        fields = ('username', 'email', 'password', 'confirm_pass');
+        fields = ('username', 'email', 'password1', 'password2');
