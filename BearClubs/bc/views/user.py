3bd6d512c480d7d604683939fdaae92748ae6a8c
Alex Hsu <alexhsu92@gmail.com>
2014-03-12 16:02:23 -0700

    added registration view

--- BearClubs/bc/views/user.py
+++ BearClubs/bc/views/user.py
@@ -1,4 +1,22 @@
 from django.shortcuts import render
+from django.contrib import auth
+from django.core.context_processors import csrf
+from BearClubs.bc.forms.user import UserSignUpForm
 
 def userSignUp(request):
-    return render(request, 'signup.html');
+	if request.method == 'POST':
+		form = UserSignUpForm(request.POST)
+		if form.is_valid():
+			form.save()
+			return render(request, 'index.html')
+			# return successful registration, should login user and redirect to club directory
+		else:
+			# find out which fields were invalid and return error
+			args = {}
+			args['form'] = form
+			return render(request, 'signup.html', args)
+	else:
+		args = {}
+		args.update(csrf(request))
+		args['form'] = UserSignUpForm()
+		return render(request, 'signup.html', args)
