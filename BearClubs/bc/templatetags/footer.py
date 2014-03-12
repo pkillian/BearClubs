ce3f47ac7ed7091452758cc709af9c6f11d709dd
Patrick Killian <pkillian@berkeley.edu>
2014-03-11 21:14:38 -0700

    Refactor completed for layout/ templates

--- BearClubs/bc/templatetags/footer.py
+++ BearClubs/bc/templatetags/footer.py
@@ -3,7 +3,7 @@ from datetime import datetime, date
 
 register = template.Library()
 
-@register.inclusion_tag('footer.html')
+@register.inclusion_tag('layout/footer.html')
 def show_footer():
     """ Render footer template with given parameters """
     return {};
--- BearClubs/bc/templatetags/header.py
+++ BearClubs/bc/templatetags/header.py
@@ -3,7 +3,7 @@ from datetime import datetime, date
 
 register = template.Library()
 
-@register.inclusion_tag('header.html')
+@register.inclusion_tag('layout/header.html')
 def show_header():
     """ Render header template with given parameters """
     return {};
