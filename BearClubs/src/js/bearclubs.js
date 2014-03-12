f8d67a1d6513cb40ff1f410d6957f67c5cbce8fb
Patrick Killian <pkillian@berkeley.edu>
2014-03-11 20:52:35 -0700

    Fix header scroll/mouseover bug

--- BearClubs/src/js/bearclubs.js
+++ BearClubs/src/js/bearclubs.js
@@ -6,38 +6,47 @@ var BearClubs = BearClubs || {};
     // false == 60 >= scrollTop > 0
     // true  == scrollTop > 60
     this.scrolled = false;
+    this.mousedOver = false;
     this.headerShown = true;
     this.headerHeight = 60;
+    this.photoScrollers = {};
 
     this.bindHeaderMouseOver = function(ev) {
+        that.mousedOver = (ev.clientY <= that.headerHeight + 20);
+
         // If the page is scrolled, the header isn't shown, and the mouse is between 0-80...
-        if (that.scrolled && !that.headerShown && ev.clientY <= that.headerHeight + 20) {
+        if (that.scrolled && !that.headerShown && that.mousedOver) {
 
             // Show the header!
             that.toggleHeader(true);
 
         // If the page is scrolled, the header is shown, and the mouse is between 81-inf...
-        } else if (that.scrolled && that.headerShown && ev.clientY > that.headerHeight + 20) {
-            
+        } else if (that.scrolled && that.headerShown && !that.mousedOver) {
+
             // Hide the header
             that.toggleHeader(false);
         }
     };
 
-    this.bindHeaderScroll = function() {
+    this.bindHeaderScroll = function(ev) {
+        that.scrolled = ($(window).scrollTop() > that.headerHeight);
+
+        if (that.mousedOver) {
+            return false;
+        }
+
         // If not scrolled past the header already, and the window scrolls past the header...
-        if (!that.scrolled && $(window).scrollTop() > that.headerHeight) {
-            
+        if (that.headerShown && that.scrolled) {
+
             // Hide the header!
             that.toggleHeader(false);
-            that.scrolled = true;
 
-        // If scrolled past the header alread, and the window has scrolled above the header...
-        } else if (that.scrolled && $(window).scrollTop() <= that.headerHeight) {
-            
+        // If scrolled past the header already, and the window has scrolled above the header...
+        } else if (!that.headerShown && !that.scrolled) {
+
             // Show the header!
             that.toggleHeader(true);
-            that.scrolled = false;
+
         }
     };
 
