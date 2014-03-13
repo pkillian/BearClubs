92619dcca15f2fece9dc46adfa907b0c14c0138a
Patrick Killian <pkillian@berkeley.edu>
2014-03-12 21:17:45 -0700

    Fix User register bug; refactor CSS to LESS; brand.berkeley.edu colors; fix tabs in signup.html file

--- BearClubs/bc/views/user.py
+++ BearClubs/bc/views/user.py
@@ -1,22 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib import auth
 from django.core.context_processors import csrf
 from BearClubs.bc.forms.user import UserSignUpForm
 
 def userSignUp(request):
+	args = {};
+
 	if request.method == 'POST':
-		form = UserSignUpForm(request.POST)
+		form = UserSignUpForm(request.POST);
 		if form.is_valid():
-			form.save()
-			return render(request, 'index.html')
 			# return successful registration, should login user and redirect to club directory
+			form.save();
+			return render(request, 'index.html');
 		else:
 			# find out which fields were invalid and return error
-			args = {}
-			args['form'] = form
-			return render(request, 'signup.html', args)
+			args['signup_form'] = form;
+			return render(request, 'signup.html', args);
 	else:
-		args = {}
-		args.update(csrf(request))
-		args['form'] = UserSignUpForm()
-		return render(request, 'signup.html', args)
+		args.update(csrf(request));
+		args['signup_form'] = UserSignUpForm();
+		return render(request, 'signup.html', args);
