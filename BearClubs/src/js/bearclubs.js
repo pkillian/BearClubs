90513ed9d0e9acdca0d9f54022ece17d2c7e21c9
Patrick Killian <pkillian@berkeley.edu>
2014-03-13 21:59:26 -0700

    Add jQuery UI and tablesorter

--- BearClubs/src/js/bearclubs.js
+++ BearClubs/src/js/bearclubs.js
@@ -66,9 +66,41 @@ var BearClubs = BearClubs || {};
         that.headerShown = toggle;
     };
 
+    $.fn.styleTable = function (options) {
+        var defaults = {
+            css: 'styleTable'
+        };
+
+        options = $.extend(defaults, options);
+
+        return this.each(function () {
+
+            var input = $(this);
+            input.addClass(options.css);
+
+            input.find("tr").on('mouseover mouseout', function (event) {
+                if (event.type === 'mouseover') {
+                    $(this).children("td").addClass("ui-state-hover");
+                } else {
+                    $(this).children("td").removeClass("ui-state-hover");
+                }
+            });
+
+            input.find("th").addClass("ui-state-default");
+            input.find("td").addClass("ui-widget-content");
+
+            input.find("tr").each(function () {
+                $(this).children("td:not(:first)").addClass("first");
+                $(this).children("th:not(:first)").addClass("first");
+            });
+        });
+    };
+
     this.onReady = function() {
         $(window).scroll(that.bindHeaderScroll);
         $(window).mousemove(that.bindHeaderMouseOver);
+        $('#club-directory').styleTable();
+        $('#club-directory').tablesorter({widgets: ['zebra']});
     };
 
 }).call(BearClubs, BearClubs, jQuery, window, document);
