41161fe4cc45b342e171fded2404ee5815aa14e8
Patrick Killian <pkillian@berkeley.edu>
2014-03-29 16:01:23 -0400

    Starting custom subscription view framework

--- BearClubs/bc/models/event.py
+++ BearClubs/bc/models/event.py
@@ -1,31 +1,32 @@
-import datetime
+from django.utils import timezone
 
 from django.db import models
 
 class Event(models.Model):
-    name           = models.CharField(max_length=128, unique=True);
+    name           = models.CharField(max_length=128);
     description    = models.TextField(blank=True);
+    organization   = models.ForeignKey('Organization');
     start_time     = models.DateTimeField();
     end_time       = models.DateTimeField();
     location       = models.CharField(max_length=256, blank=True);
     location_lat   = models.CharField(max_length=256, blank=True);
     location_lng   = models.CharField(max_length=256, blank=True);
-    contact_email  = models.EmailField(max_length=128, unique=True);
-    created_at   = models.DateTimeField(editable=False);
-    updated_at   = models.DateTimeField();
+    contact_email  = models.EmailField(max_length=128);
+    created_at     = models.DateTimeField(default=timezone.now, editable=False);
+    updated_at     = models.DateTimeField(default=timezone.now);
 
     def save(self, *args, **kwargs):
         ''' On save, update timestamps '''
-        datetime_now = datetime.datetime.now();
+        datetime_now = timezone.now();
 
         # If there's no ID, it's new
         if not self.id:
             self.created_at = datetime_now;
 
         # Always update the modified at value
-        self.modified_at = datetime_now;
+        self.updated_at = datetime_now;
 
-        return super(User, self).save(*args, **kwargs);
+        return super(Event, self).save(*args, **kwargs);
 
     class Meta:
         app_label = 'bc';
--- BearClubs/bc/models/mappings/user.py
+++ BearClubs/bc/models/mappings/user.py
@@ -2,14 +2,40 @@ import datetime
 
 from django.db import models
 
-from BearClubs.bc.models.user import User
+from BearClubs.bc.models.event import Event
 from BearClubs.bc.models.organization import Organization
+from BearClubs.bc.models.user import User
 
 class UserToOrganization(models.Model):
     user         = models.ManyToManyField(User);
     organization = models.ManyToManyField(Organization);
+    admin        = models.BooleanField(default=False);
     created_at   = models.DateTimeField(editable=False);
     updated_at   = models.DateTimeField();
+
+    def save(self, *args, **kwargs):
+        ''' On save, update timestamps '''
+        datetime_now = datetime.datetime.now();
+
+        # If there's no ID, it's new
+        if not self.id:
+            self.created_at = datetime_now;
+
+        # Always update the modified at value
+        self.modified_at = datetime_now;
+
+        return super(User, self).save(*args, **kwargs);
+
+    class Meta:
+        app_label = 'bc';
+        db_table  = 'bc_user_to_organization';
+
+class UserToEvent(models.Model):
+    user       = models.ManyToManyField(User);
+    event      = models.ManyToManyField(Event);
+    admin      = models.BooleanField(default=False);
+    created_at = models.DateTimeField(editable=False);
+    updated_at = models.DateTimeField();
 
     def save(self, *args, **kwargs):
         ''' On save, update timestamps '''
