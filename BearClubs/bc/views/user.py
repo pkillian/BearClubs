d329f3082ac128f6a4e131a23cdb1db90ee2a52b
Patrick Killian <pkillian@berkeley.edu>
2014-03-30 19:52:55 -0400

    Convert tabs to spaces

--- BearClubs/bc/views/user.py
+++ BearClubs/bc/views/user.py
@@ -4,58 +4,58 @@ from django.core.context_processors import csrf
 from BearClubs.bc.forms.user import UserSignUpForm, UserSignInForm
 
 def userSignUp(request):
-	if request.method == 'POST':
-		form = UserSignUpForm(request.POST)
-		if form.is_valid():
-			# return successful registration, should login user and redirect to club directory
-			user = form.save()
-			auth.login(request, user)
-			return redirect("/clubs")
-		else:
-			# find out which fields were invalid and return error
-			return userFormsRender(request, signUpForm=form)
-			
-	else:
-		if (request.user.is_authenticated()):
-			# if user is already authenticated then redirect to club directory
-			return redirect("/clubs")
-		else:
-			return userFormsRender(request)
+    if request.method == 'POST':
+        form = UserSignUpForm(request.POST)
+        if form.is_valid():
+            # return successful registration, should login user and redirect to club directory
+            user = form.save()
+            auth.login(request, user)
+            return redirect("/clubs")
+        else:
+            # find out which fields were invalid and return error
+            return userFormsRender(request, signUpForm=form)
+
+    else:
+        if (request.user.is_authenticated()):
+            # if user is already authenticated then redirect to club directory
+            return redirect("/clubs")
+        else:
+            return userFormsRender(request)
 
 def userSignIn(request):
-	if request.method == 'POST':
-		form = UserSignInForm(data=request.POST);
+    if request.method == 'POST':
+        form = UserSignInForm(data=request.POST);
 
-		if form.is_valid():
-			form.loginUser(request);
+        if form.is_valid():
+            form.loginUser(request);
 
-			# On successful authentication, redirect to club directory
-			return redirect("/clubs");
-		else:
-			# On unsuccessful authentication, return error
-			return userFormsRender(request, signInForm=form);
-	else:
-		if (request.user.is_authenticated()):
-			# if user is already authenticated then redirect to club directory
-			return redirect("/clubs")
-		else:
-			return userFormsRender(request);
+            # On successful authentication, redirect to club directory
+            return redirect("/clubs");
+        else:
+            # On unsuccessful authentication, return error
+            return userFormsRender(request, signInForm=form);
+    else:
+        if (request.user.is_authenticated()):
+            # if user is already authenticated then redirect to club directory
+            return redirect("/clubs")
+        else:
+            return userFormsRender(request);
 
 def userLogOut(request):
-	auth.logout(request)
-	return render(request, 'index.html')
+    auth.logout(request)
+    return render(request, 'index.html')
 
 def userFormsRender(request, signUpForm=None, signInForm=None):
-	args = {};
-	args.update(csrf(request));
+    args = {};
+    args.update(csrf(request));
 
-	if signUpForm == None:
-		signUpForm = UserSignUpForm();
+    if signUpForm == None:
+        signUpForm = UserSignUpForm();
 
-	if signInForm == None:
-		signInForm = UserSignInForm();
+    if signInForm == None:
+        signInForm = UserSignInForm();
 
-	args['signup_form'] = signUpForm;
-	args['signin_form'] = signInForm;
+    args['signup_form'] = signUpForm;
+    args['signin_form'] = signInForm;
 
-	return render(request, 'signup.html', args);
+    return render(request, 'signup.html', args);
