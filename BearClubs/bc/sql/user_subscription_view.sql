66e9c3adf5202ea46351647a6bf1ed3164ee428f
Patrick Killian <pkillian@berkeley.edu>
2014-03-30 18:50:07 -0400

    Init custom view pSQL

---
+++ BearClubs/bc/sql/user_subscription_view.sql
@@ -0,0 +1,57 @@
+/*
+    In this view, we create a subscription object structure to return, select
+    all items that are related to the User via the associated mappings 
+    (UserToOrganization, UserToEvent, etc.), and include logical indications
+    in the subscription object to descirbe what type of object the row in the
+    return data is.
+
+    For example, if User1 has two subscriptions, Org1 and Event1, the return
+    data should be of a form similar to:
+
+    ```
+    SELECT * FROM bc_user_subscription_view WHERE username = 'User1';
+
+    Field                           | Data
+    -----------------------------------------------------------------
+    user_id                         | 1
+    user_username                   | 'User1'
+    ...                             | ...
+    subscription_type               | 0
+    subscription_type_description   | 'Organization'
+    subscription_id                 | 1
+    subscription_name               | 'Org1'
+    ...                             | ...
+    -----------------------------------------------------------------
+    user_id                         | 1
+    user_username                   | 'User1'
+    ...                             | ...
+    subscription_type               | 1
+    subscription_type_description   | 'Event'
+    subscription_id                 | 1
+    subscription_name               | 'Event1'
+    ...                             | ...
+    
+    ```
+
+    This return data should encapsulate all possible commonalities between
+    object types. The calling section of code ideally should NOT have to make
+    further calls to the DB to get more data; this adds unnessecary I/O. 
+
+*/
+
+-- Create return data structure here
+
+-- DROP IF EXISTS bc_user_subscription_view;
+
+/* 
+CREATE VIEW bc_user_subscription_view
+
+    SELECT INTO `return data structure name` 
+    FROM bc_user_to_organization 
+    WHERE user_id = `user id variable`;
+
+    SELECT INTO `return data structure name` 
+    FROM bc_user_to_event
+    WHERE user_id = `user id variable`;
+;
+*/
