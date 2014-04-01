68250c4cb5c9a8446abeeae97b8ba8550a0b24e0
Kevin Chen <kevinchen8223@gmail.com>
2014-04-01 11:27:01 -0700

    added datetimewidget

--- BearClubs/bc/forms/event.py
+++ BearClubs/bc/forms/event.py
@@ -1,13 +1,23 @@
 from django import forms
 from BearClubs.bc.models import Event
+from datetimewidget.widgets import DateTimeWidget
 
 class AddEventForm(forms.ModelForm):
 
     def __init__(self, user, *args, **kwargs):
         forms.ModelForm.__init__(self, *args, **kwargs);
+        
+        dateTimeOptions = {
+             'format': 'mm/dd/yyyy HH:ii P',
+             'autoclose': 'true',
+             'showMeridian' : 'true'
+        }
+
 
         self.user = user;
         self.fields['contact_email'].initial = user.email;
+        self.fields['start_time'].widget = DateTimeWidget(options = dateTimeOptions)
+        self.fields['end_time'].widget = DateTimeWidget(options = dateTimeOptions)
 
     class Meta:
         model   = Event;
