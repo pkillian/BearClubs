e4c3f89020c7068da5ff5f3fe3a98744c21aa673
Patrick Killian <pkillian@berkeley.edu>
2014-04-02 11:23:17 -0700

    Fix __init__ declarations for finding classes

--- BearClubs/bc/models/__init__.py
+++ BearClubs/bc/models/__init__.py
@@ -1,7 +1,7 @@
 from user import User
 from event import Event
 from organization import Organization, OrganizationType
-from mappings import UserToOrganization
+from mappings import UserToEvent, UserToOrganization
 from views import UserSubscriptionView
 
-__all__ = ['User', 'Event', 'Organization', 'OrganizationType', 'UserToOrganization', 'UserSubscriptionView'];
+__all__ = ['User', 'Event', 'Organization', 'OrganizationType', 'UserToEvent', 'UserToOrganization', 'UserSubscriptionView'];
