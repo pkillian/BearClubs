26e6bbd79f20a2a0532f0bc650df497557d81a7c
Patrick Killian <pkillian@berkeley.edu>
2014-04-01 18:34:35 -0700

    Fixed mappings

--- BearClubs/bc/models/mappings/user.py
+++ BearClubs/bc/models/mappings/user.py
@@ -1,21 +1,20 @@
-import datetime
-
 from django.db import models
+from django.utils import timezone
 
 from BearClubs.bc.models.event import Event
 from BearClubs.bc.models.organization import Organization
 from BearClubs.bc.models.user import User
 
 class UserToOrganization(models.Model):
-    user         = models.ManyToManyField(User);
+    user         = models.ForeignKey(User);
     organization = models.ManyToManyField(Organization);
     admin        = models.BooleanField(default=False);
-    created_at   = models.DateTimeField(editable=False);
-    updated_at   = models.DateTimeField();
+    created_at   = models.DateTimeField(default=timezone.now, editable=False);
+    updated_at   = models.DateTimeField(default=timezone.now);
 
     def save(self, *args, **kwargs):
         ''' On save, update timestamps '''
-        datetime_now = datetime.datetime.now();
+        datetime_now = timezone.now();
 
         # If there's no ID, it's new
         if not self.id:
@@ -24,22 +23,22 @@ class UserToOrganization(models.Model):
         # Always update the modified at value
         self.modified_at = datetime_now;
 
-        return super(User, self).save(*args, **kwargs);
+        return super(UserToOrganization, self).save(*args, **kwargs);
 
     class Meta:
         app_label = 'bc';
         db_table  = 'bc_user_to_organization';
 
 class UserToEvent(models.Model):
-    user       = models.ManyToManyField(User);
+    user       = models.ForeignKey(User);
     event      = models.ManyToManyField(Event);
     admin      = models.BooleanField(default=False);
-    created_at = models.DateTimeField(editable=False);
-    updated_at = models.DateTimeField();
+    created_at = models.DateTimeField(default=timezone.now, editable=False);
+    updated_at = models.DateTimeField(default=timezone.now);
 
     def save(self, *args, **kwargs):
         ''' On save, update timestamps '''
-        datetime_now = datetime.datetime.now();
+        datetime_now = timezone.now();
 
         # If there's no ID, it's new
         if not self.id:
@@ -48,7 +47,7 @@ class UserToEvent(models.Model):
         # Always update the modified at value
         self.modified_at = datetime_now;
 
-        return super(User, self).save(*args, **kwargs);
+        return super(UserToEvent, self).save(*args, **kwargs);
 
     class Meta:
         app_label = 'bc';
