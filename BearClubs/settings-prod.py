bfff8cd4e192df8bc07e44cf87264d409060b6be
Peter Kim <peterkim45366@gmail.com>
2014-04-01 21:43:47 -0700

    forgot config change for elastic search

--- BearClubs/settings-prod.py
+++ BearClubs/settings-prod.py
@@ -98,7 +98,7 @@ HAYSTACK_CONNECTIONS = {
     'default': {
         'ENGINE': 'haystack.backends.elasticsearch_backend.ElasticsearchSearchEngine',
         'URL': os.environ['SEARCHBOX_URL'],
-        'INDEX_NAME': 'documents',
+        'INDEX_NAME': 'haystack',
         'INCLUDE_SPELLING': True,
     },
 }
