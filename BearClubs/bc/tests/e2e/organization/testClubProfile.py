971f30ce8c0571aac9a1ffca6b4904a392445dbb
Tiffany Ng <ng_tiffany@berkeley.edu>
2014-04-18 01:52:43 -0700

    fixed up and added club profile tests

--- BearClubs/bc/tests/e2e/organization/testClubProfile.py
+++ BearClubs/bc/tests/e2e/organization/testClubProfile.py
@@ -26,11 +26,6 @@ class clubProfileTests(TestCase):
         self.user3.set_password('1234');
         self.user3.save();
 
-
-        self.uto1 = UserToOrganization(user=self.user, organization=self.org1);
-        self.uto2 = UserToOrganization(user=self.user, organization=self.org2);
-        self.uto3 = UserToOrganization(user=self.user, organization=self.org3);
-
         self.org1 = Organization(name='test org 1', contact_email='test1@test.com', organization_type=OrganizationType.objects.get(id=1));
         self.org2 = Organization(name='test org 2', contact_email='test2@test.com', organization_type=OrganizationType.objects.get(id=2));
         self.org3 = Organization(name='test org 3', contact_email='test3@test.com', organization_type=OrganizationType.objects.get(id=1));
@@ -39,12 +34,51 @@ class clubProfileTests(TestCase):
         self.org2.save();
         self.org3.save();
 
+
+        self.uto1 = UserToOrganization(user=self.user, organization=self.org1).save();
+        self.uto2 = UserToOrganization(user=self.user2, organization=self.org1, admin=True).save();
+
     def testViewClubNotLoggedIn(self):
     	response = self.client.get(self.baseURL + '/clubs/1');
 
     	self.assertTrue(self.org1.name in response.content);
-    	self.assertTrue(self.user.username in response.content);
-    	self.assertTrue('href="/clubs/1')
+        self.assertTrue(self.org1.contact_email in response.content);
+        self.assertTrue(self.org1.organization_type.name in response.content);
+        self.assertFalse("Join Club" in response.content);
+        self.assertFalse("Manage" in response.content);
+
+    def testViewClubLoggedInNotMember(self):
+        self.client.login(username='test3', password='1234');
+
+        response = self.client.get(self.baseURL + '/clubs/1');
+
+        self.assertTrue(self.org1.name in response.content);
+        self.assertTrue(self.org1.contact_email in response.content);
+        self.assertTrue(self.org1.organization_type.name in response.content);
+        self.assertTrue("Join Club" in response.content);
+        self.assertFalse("Manage" in response.content);
+
+    def testViewClubLoggedInMember(self):
+        self.client.login(username='test', password='1234');
+
+        response = self.client.get(self.baseURL + '/clubs/1');
+
+        self.assertTrue(self.org1.name in response.content);
+        self.assertTrue(self.org1.contact_email in response.content);
+        self.assertTrue(self.org1.organization_type.name in response.content);
+        self.assertTrue("You are a member of this club." in response.content);
+        self.assertFalse("Manage" in response.content);
+
+    def testViewClubLoggedInAdmin(self):
+        self.client.login(username='test2', password='1234');
+
+        response = self.client.get(self.baseURL + '/clubs/1');
+
+        self.assertTrue(self.org1.name in response.content);
+        self.assertTrue(self.org1.contact_email in response.content);
+        self.assertTrue(self.org1.organization_type.name in response.content);
+        self.assertTrue("You are a member and admin of this club." in response.content);
+        self.assertTrue("Manage" in response.content);
 
     def tearDown(self):
         self.client.logout();
