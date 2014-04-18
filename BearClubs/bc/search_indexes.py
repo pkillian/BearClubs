57ca2982831c2f4ebbbc673294658e3aa232c17c
Peter Kim <peterkim45366@gmail.com>
2014-04-18 00:50:17 -0700

    set up search indexes for auto complete // added front end js handler

--- BearClubs/bc/search_indexes.py
+++ BearClubs/bc/search_indexes.py
@@ -9,6 +9,12 @@ class UserIndex(indexes.SearchIndex, indexes.Indexable):
     first_name = indexes.CharField(model_attr='first_name')
     last_name = indexes.CharField(model_attr='last_name')
 
+    # We add this for autocomplete.
+    first_name_auto = indexes.EdgeNgramField(model_attr='first_name')
+    last_name_auto = indexes.EdgeNgramField(model_attr='last_name')
+    username_auto = indexes.EdgeNgramField(model_attr='username')
+    email_auto = indexes.EdgeNgramField(model_attr='email')
+
     def get_model(self):
         return user.User
 
@@ -18,6 +24,10 @@ class EventIndex(indexes.SearchIndex, indexes.Indexable):
     description = indexes.CharField(model_attr='description')
     organization = indexes.CharField(model_attr='organization')
 
+    name_auto = indexes.EdgeNgramField(model_attr='name')
+    description_auto = indexes.EdgeNgramField(model_attr='description')
+    organization_auto = indexes.EdgeNgramField(model_attr='organization')
+
     def get_model(self):
         return event.Event
 
@@ -26,6 +36,10 @@ class OrganizationIndex(indexes.SearchIndex, indexes.Indexable):
     name = indexes.CharField(model_attr='name')
     description = indexes.CharField(model_attr='description')
     location = indexes.CharField(model_attr='location')
+
+    name_auto = indexes.EdgeNgramField(model_attr='name')
+    description_auto = indexes.EdgeNgramField(model_attr='description')
+    location_auto = indexes.EdgeNgramField(model_attr='location')
 
     def get_model(self):
         return organization.Organization
